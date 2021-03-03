ade76caf5a731642afe1d109a455d9a9071f9f71
Guilherme Estephano <guiestephano@gmail.com>
2021-03-03 15:13:04 -0300

    Exercicio pronto

---
+++ CoordenadaPonto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CoordenadaPonto {
+    public static void main(String [] args){
+        float X, Y;
+        Scanner coordenada = new Scanner (System.in);
+        X = coordenada.nextFloat();
+        Y = coordenada.nextFloat();
+        if(X > 0 && Y > 0){
+            System.out.println("Q1");
+        }
+        else if(X < 0 && Y > 0){
+            System.out.println("Q2");
+        }
+        else if (X < 0 && Y < 0){
+            System.out.println("Q3");
+        }
+        else if (X > 0 && Y < 0){
+            System.out.println("Q4");
+        }
+        else if (X == 0 && Y == 0){
+            System.out.println("Origem");
+        }
+    }
+}
